6dadf7b5b6f2790be3dae9f4938c4e139d96550f
ijosue16 <iradukundajosue16@gmail.com>
2022-12-24 15:49:34 +0200

    update fix

--- src/scripts/todolist.js
+++ src/scripts/todolist.js
@@ -1,10 +1,10 @@
-const taskHolder = document.querySelector('.task-holder');
-const input = document.querySelector('.addinput');
-const clearing = document.querySelector('.clearing');
+const taskHolder = document.querySelector(".task-holder");
+const input = document.querySelector(".addinput");
+const clearing = document.querySelector(".clearing");
 
 class TodolistClass {
   constructor() {
-    this.taskInfo = JSON.parse(localStorage.getItem('Todo')) || [];
+    this.taskInfo = JSON.parse(localStorage.getItem("Todo")) || [];
   }
 
   addTask(doTask) {
@@ -14,7 +14,7 @@ class TodolistClass {
       index: this.taskInfo.length,
     };
     this.taskInfo.push(todoobj);
-    localStorage.setItem('Todo', JSON.stringify(this.taskInfo));
+    localStorage.setItem("Todo", JSON.stringify(this.taskInfo));
     return todoobj;
   }
 
@@ -23,46 +23,46 @@ class TodolistClass {
   }
 
   static displayTask(wee) {
-    const taskElement = document.createElement('li');
+    const taskElement = document.createElement("li");
     taskElement.classList.add(
-      'px-0',
-      'd-flex',
-      'align-items-center',
-      'py-1',
-      'px-2',
-      'task-element',
-      'border-bottom',
+      "px-0",
+      "d-flex",
+      "align-items-center",
+      "py-1",
+      "px-2",
+      "task-element",
+      "border-bottom"
     );
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('checkbox');
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
 
-    const taskDescription = document.createElement('p');
+    const taskDescription = document.createElement("p");
     taskDescription.classList.add(
-      'px-2',
-      'align-items-center',
-      'm-0',
-      'container',
-      'fs-5',
+      "px-2",
+      "align-items-center",
+      "m-0",
+      "container",
+      "fs-5"
     );
     taskDescription.innerHTML = wee.description;
     if (wee.completed === true) {
       checkbox.checked = true;
-      taskDescription.classList.add('text-decoration-line-through');
+      taskDescription.classList.add("text-decoration-line-through");
     } else {
       checkbox.checked = false;
-      taskDescription.classList.add('text-decoration-none');
+      taskDescription.classList.add("text-decoration-none");
     }
 
-    const dots = document.createElement('span');
-    dots.classList.add('dots');
+    const dots = document.createElement("span");
+    dots.classList.add("dots");
     dots.innerHTML = `<svg class="text-secondary text-opacity-10" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
         <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
       </svg>`;
 
-    const bin = document.createElement('span');
-    bin.classList.add('d-none', 'bin');
+    const bin = document.createElement("span");
+    bin.classList.add("d-none", "bin");
     bin.innerHTML = `<svg class="text-secondary text-opacity-10" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -82,30 +82,30 @@ class TodolistClass {
 
   // updateTaskStatus
   updateTaskStatus(chck, index) {
-    chck.addEventListener('click', (r) => {
+    chck.addEventListener("click", (r) => {
       if (r.currentTarget.checked) {
-        chck.nextElementSibling.classList.add('text-decoration-line-through');
+        chck.nextElementSibling.classList.add("text-decoration-line-through");
         this.taskInfo[index].completed = true;
-        localStorage.setItem('Todo', JSON.stringify(this.taskInfo));
+        localStorage.setItem("Todo", JSON.stringify(this.taskInfo));
       } else {
         if (
           chck.nextElementSibling.classList.contains(
-            'text-decoration-line-through',
+            "text-decoration-line-through"
           )
         ) {
           chck.nextElementSibling.classList.remove(
-            'text-decoration-line-through',
+            "text-decoration-line-through"
           );
         }
         this.taskInfo[index].completed = false;
-        localStorage.setItem('Todo', JSON.stringify(this.taskInfo));
+        localStorage.setItem("Todo", JSON.stringify(this.taskInfo));
         TodolistClass.reloading();
       }
     });
   }
 
   removeSelectedTask(trash, index) {
-    trash.addEventListener('click', () => {
+    trash.addEventListener("click", () => {
       taskHolder.removeChild(trash.parentElement);
       this.taskInfo.splice(index, 1);
       this.taskInfo.forEach((list) => {
@@ -113,7 +113,7 @@ class TodolistClass {
           list.index -= 1;
         }
       });
-      localStorage.setItem('Todo', JSON.stringify(this.taskInfo));
+      localStorage.setItem("Todo", JSON.stringify(this.taskInfo));
       TodolistClass.reloading();
     });
   }
@@ -121,14 +121,14 @@ class TodolistClass {
   // filter
 
   remove(cl) {
-    cl.addEventListener('click', () => {
+    cl.addEventListener("click", () => {
       this.taskInfo = this.taskInfo.filter((cls) => cls.completed !== true);
       this.taskInfo.forEach((remain, index) => {
         if (remain.index !== index) {
           remain.index = index;
         }
       });
-      localStorage.setItem('Todo', JSON.stringify(this.taskInfo));
+      localStorage.setItem("Todo", JSON.stringify(this.taskInfo));
       TodolistClass.reloading();
     });
   }
@@ -140,27 +140,27 @@ const tododisplay = new TodolistClass();
 
 function runfunction() {
   tododisplay.showTask();
-  input.addEventListener('keypress', (e) => {
-    if (input.value && e.key === 'Enter') {
+  input.addEventListener("keypress", (e) => {
+    if (input.value && e.key === "Enter") {
       const newaddedTask = tododisplay.addTask(input.value);
       TodolistClass.displayTask(newaddedTask);
-      input.value = '';
+      input.value = "";
       e.preventDefault();
       TodolistClass.reloading();
     }
   });
 
-  const dots = document.querySelectorAll('.dots');
+  const dots = document.querySelectorAll(".dots");
 
   dots.forEach((ddt, index) => {
-    ddt.addEventListener('click', () => {
-      ddt.classList.add('d-none');
-      ddt.nextElementSibling.classList.remove('d-none');
+    ddt.addEventListener("click", () => {
+      ddt.classList.add("d-none");
+      ddt.nextElementSibling.classList.remove("d-none");
       tododisplay.removeSelectedTask(ddt.nextElementSibling, index);
     });
   });
 
-  const checkbox = document.querySelectorAll('.checkbox');
+  const checkbox = document.querySelectorAll(".checkbox");
 
   checkbox.forEach((chkbx, index) => {
     tododisplay.updateTaskStatus(chkbx, index);
